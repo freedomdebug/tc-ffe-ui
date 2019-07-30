78c0ecd0995cb503d076ed94b1c84ff57e00de55
ttommy.hu <tommy.hu@ly.com>
2019-07-30 09:06:29 +0800

    first commit

---
+++ examples/components/header.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="page-header">
+    <div class="logo">
+      <img src="../assets/logo.png">TC-FFE-UI
+    </div>
+    <!-- <div class="ui" >tc-ffe-ui</div> -->
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+  .page-header{
+    height: 70px;
+    box-shadow: rgba(29, 29, 29, 0.07) 0px 10px 60px 0px;
+  }
+  .page-header .logo{
+    float: left;
+    margin-left: 60px;
+    padding-top:20px;
+    font-size: 48px;
+    font-weight: bold;
+    color: #3f536e;
+  }
+  .page-header .logo img{
+    margin-top: 10px;
+    width: 80px;
+    height: 65px;
+    margin-left: 65px;
+    margin-right: 5px;
+    vertical-align: text-bottom;
+  }
+  .page-header .ui{
+    float: left;
+    color: #6190e8;
+    margin-top: 43px;
+    margin-left: 8px;
+    font-size: 13px
+  }
+</style>
---
+++ packages/README.md
@@ -0,0 +1,14 @@
+## Introduction
+> A Vue.js 2.0 UI Toolkit for Web
+
+## Install
+`npm install tc-ffe-ui --save`
+
+## Quick Start
+`import Vue from 'vue'`<br/>
+`import JKUI from 'tc-ffe-ui'`<br/>
+`import 'tc-ffe-ui/theme-default/lib/index.css'`<br/>
+`Vue.use(JKUI)`
+
+## Author
+>Jike
